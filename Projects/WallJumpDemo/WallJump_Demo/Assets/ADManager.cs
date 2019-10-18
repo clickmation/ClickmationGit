f9bc98fe2dbae719f1cf2414c17692a0b9c111eb
oCreaCo <gad04250@gmail.com>
2019-10-18 16:57:49 +0900

    2019-10-18-16-57-ADManager Added

---
+++ Projects/WallJumpDemo/WallJump_Demo/Assets/ADManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class ADManager : MonoBehaviour
+{
+    public static ADManager instance;
+
+    private string appID = "ca-app-pub-3575537359490368~8192022467";
+
+    private BannerView bannerView;
+    private string bannerID = "ca-app-pub-3940256099942544/6300978111";
+
+    private InterstitialAd fullScreenAd;
+    private string fullScreenAdID = "ca-app-pub-3940256099942544/1033173712";
+
+    private RewardBasedVideoAd rewardedAd;
+    private string rewardedAdID = "ca-app-pub-3940256099942544/5224354917";
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        MobileAds.Initialize(appID);
+
+        RequestFullScreenAd();
+
+        rewardedAd = RewardBasedVideoAd.Instance;
+
+        RequestRewardedAd();
+
+
+        rewardedAd.OnAdLoaded += HandleRewardBasedVideoLoaded;
+
+        rewardedAd.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+
+        rewardedAd.OnAdRewarded += HandleRewardBasedVideoRewarded;
+
+        rewardedAd.OnAdClosed += HandleRewardBasedVideoClosed;
+    }
+
+
+    public void RequestBanner()
+    {
+        bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+
+        bannerView.Show();
+    }
+
+    public void HideBanner()
+    {
+        bannerView.Hide();
+    }
+
+    public void RequestFullScreenAd()
+    {
+        fullScreenAd = new InterstitialAd(fullScreenAdID);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        fullScreenAd.LoadAd(request);
+
+    }
+
+    public void ShowFullScreenAd()
+    {
+        if (fullScreenAd.IsLoaded())
+        {
+            fullScreenAd.Show();
+            RequestFullScreenAd();
+        }
+        else
+        {
+            Debug.Log("Full screen ad not loaded");
+            RequestFullScreenAd();
+        }
+    }
+
+    public void RequestRewardedAd()
+    {
+        AdRequest request = new AdRequest.Builder().Build();
+
+        rewardedAd.LoadAd(request, rewardedAdID);
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (rewardedAd.IsLoaded())
+        {
+            rewardedAd.Show();
+        }
+        else
+        {
+            Debug.Log("Rewarded ad not loaded");
+        }
+    }
+
+
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        Debug.Log("Rewarded Video ad loaded successfully");
+
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        Debug.Log("Failed to load rewarded video ad : " + args.Message);
+
+
+    }
+
+
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        Debug.Log("You have been rewarded with  " + amount.ToString() + " " + type);
+
+        
+
+
+    }
+
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        Debug.Log("Rewarded video has closed");
+        RequestRewardedAd();
+
+    }
+}
