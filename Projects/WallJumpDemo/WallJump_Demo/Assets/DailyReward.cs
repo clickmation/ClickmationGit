ad5ae1f967261c436f362738075f6eaa4401cc78
crimonder <clickmation@gmail.com>
2019-10-28 01:46:16 +0900

    2019-10-28-01-45 Daily
    -muh gari bba geh jin da

--- Projects/WallJumpDemo/WallJump_Demo/Assets/DailyReward.cs
+++ Projects/WallJumpDemo/WallJump_Demo/Assets/DailyReward.cs
@@ -16,14 +16,20 @@ public class DailyReward : MonoBehaviour
     private TimeSpan lastDate;
     private TimeSpan _remainingTime;
     private string timeformat;
-    private bool timerSet;
     private bool countIsReady;
-    private int tokenStack = 3;
+    private int curStack;
 
+	
+	
+	//lastDate도 saver에 어케 해야할거임
+	
     void Start()
     {
         eventEndTime = TimeSpan.Parse(endTime);
         StartCoroutine("CheckTime");
+		if (StackNeedsRefresh()){
+			RefreshStack();
+		}
     }
 
     private IEnumerator CheckTime()
@@ -43,32 +49,87 @@ public class DailyReward : MonoBehaviour
     {
         currentTime = TimeSpan.Parse(TimeManager.sharedInstance.getCurrentTimeNow());
         currentDate = TimeSpan.Parse(TimeManager.sharedInstance.getCurrentDateNow());
-        timerSet = true;
+        curStack = saver.curStack;
     }
 
     void Update()
     {
-        if (timerSet)
-        {
-            CheckIfTokenNeedsRefresh();
-            if (tokenStack > 0)
-            {
-
-            }
-            else
-            {
-
-            }
-        }
+		if (!isFullStack) {
+			_remainingTime = eventEndTime.Subtract(currentTime);
+			tcounter = _remainingTime.TotalMilliseconds;
+			countIsReady = true;
+		}
+		
+		if (countIsReady) {startCountdown();}
     }
+	
+	public string GetRemainingTime(double x)
+	{
+		TimeSpan tempB = TimeSpan.FromMilliseconds(x);
+		Timeformat = string.Format("{0:D2}:{1:D2}:{2:D2}", tempB.Hours, tempB.Minutes, tempB.Seconds);
+		return Timeformat
+	}
+	
+	private void startCountdown()
+	{
+		tcounter-= Time.deltaTime * 1000;
+		deactivateButton(GetRemainingTime(tcounter) + "Until\nMore Revive Tokens");
+		
+		if (tcounter <= 0) {
+			countIsReady = false;
+			validateTime ();
+		}
+	}
 
-    public void CheckIfTokenNeedsRefresh()
+    public bool StackNeedsRefresh()
     {
-        if (currentDate.Subtract(lastDate).TotalDays > 0)
-        {
-            tokenStack = 3;
-            updateTime();
-            lastDate = currentDate;
-        }
+        if (currentDate.Subtract(lastDate).TotalDays > 0) {
+            return true;
+        } else {
+			return false;
+		}
     }
+	
+	public bool IsFullStack()
+	{
+		if (saver.curStack >= saver.maxStack) {
+			activateButton("")
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void refreshStack()
+	{
+		lastDate = currentDate;
+		//curStack의 값을 maxStack의 값으로 바꿈 (maxStack은 saver에 있음)
+	}
+	
+	public void getReward()
+	{
+		saver.curStack--;
+		//token갯수 ++
+		updateTime();
+	}
+	
+	private void activateButton(string x)
+	{
+		tokenButton.interactable = true;
+		//animation 그거 parameter 중 activated를 true로 하면 될거임
+		timeLabel.text = x;
+	}
+	
+	private void deactivateButton(string x)
+	{
+		tokenButton.interactable = false;
+		//animation 그거 parameter 중 activated를 false로 하면 될거임
+		timeLabel.text = x;
+	}
+	
+	private void validateTime()
+	{
+		Debug.Log ("==> Validating time to make sure no speed hack!");
+        StartCoroutine ("CheckTime");
+	}
 }
