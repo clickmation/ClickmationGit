428fa5485102d1a2468a88fdd76c214a44839ac9
crimonder <clickmation@gmail.com>
2019-11-12 15:26:50 +0900

    Points N Edges

---
+++ Projects/PointsNEdges/New Unity Project/Assets/GameMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+	
+	[SerializeField] private Transform player;
+	public GameObject[] projectiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void TurnRight ()
+	{
+		player.localScale = new Vector3(1.0f,1.0f,1.0f);
+	}
+	
+	public void TurnLeft ()
+	{
+		player.localScale = new Vector3(-1.0f,1.0f,1.0f);
+	}
+}
---
+++ Projects/PointsNEdges/New Unity Project/Assets/projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+	public float speed = 5;
+	
+	private void Update ()
+	{
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+	}
+}
