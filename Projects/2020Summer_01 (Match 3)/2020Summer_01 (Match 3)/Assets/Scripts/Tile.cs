c441013ef344bb410bce9ad8e1e5901e1f69a442
crimonder <clickmation@gmail.com>
2020-07-06 16:00:49 +0900

    2020SS01_20200706
    시작

---
+++ Projects/2020Summer_01 (Match 3)/2020Summer_01 (Match 3)/Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    Board m_board;
+
+    public void Init(int x, int y, Board board)
+    {
+        xIndex = x;
+        yIndex = y;
+        m_board = board;
+    }
+
+    void OnMouseDown()
+    {
+        if (m_board != null)
+        {
+            m_board.ClickTile(this);
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (m_board != null)
+        {
+            m_board.DragToTile(this);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (m_board != null)
+        {
+            m_board.ReleaseTile();
+        }
+    }
+}
